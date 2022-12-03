fa833a4f9a3856298300b4ffac5b9de6acf78490
sanjaythiru <stsanjay807@gmail.com>
2022-12-03 08:28:05 +0530

    initial commit

---
+++ src/main/java/com/sanjaythiru/service/lichessapiexperiments/LichessApiExperimentsApplication.java
@@ -0,0 +1,13 @@
+package com.sanjaythiru.service.lichessapiexperiments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LichessApiExperimentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LichessApiExperimentsApplication.class, args);
+	}
+
+}
